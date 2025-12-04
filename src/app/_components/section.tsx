1a8cd2eb0449aed1e6224e66f21f6e11a9761877
Marcos Napolitano <marcosnapolitano@hotmail.com.ar>
2025-12-04 20:59:03 +0100

    Update Catalog View

--- src/app/_components/section.tsx
+++ src/app/_components/section.tsx
@@ -13,17 +13,11 @@ export const Section: React.FC<ISection> = ({ id, name, user, children }) => {
   return (
     <section id={ name + "-" + id } className={`${name} section`} style={{ backgroundImage: `url(${backgroundImage.src})` }}>
         <div className={`svg section-header ${id === "1" ? "category-header" : ""}`}>
-            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-                <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" className="shape-fill"></path>
-            </svg>
             <h2 className={ `header header-` + name }>{name?.toUpperCase()}</h2>
             {id === "1" && user !== "gianfranco" && logoHeader }
         </div>
         {children}
         <div className="svg section-footer">
-            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-                <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" className="shape-fill"></path>
-            </svg>
             <footer className="footer">
               <a href={`https://wa.me/54${phone}`} className="footer-link">Recibí Nuestras Ofertas</a>
               <br />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,6 +18,25 @@ export default async function Home() {
       <CategoryIndex user={user.userName} />
       <script src="index.js" defer></script>
       {app ? app : "Did Not Mount"}
+      <nav id="navbar" className="side-menu hidden">
+        <h2 id="close-menu">X</h2>
+        <ul className="side-menu-list">
+          <li className="side-menu-item"><a href="#almacen-1">Almacén</a></li>
+          <li className="side-menu-item"><a href="#bebidas-1">Bebidas</a></li>
+          <li className="side-menu-item"><a href="#cafe-1">Café</a></li>
+          <li className="side-menu-item"><a href="#edulcorantes-1">Edulcorantes</a></li>
+          <li className="side-menu-item"><a href="#galletitas-1">Galletitas</a></li>
+          <li className="side-menu-item"><a href="#medicamentos-1">Medicamentos</a></li>
+          <li className="side-menu-item"><a href="#nucete-1">Nucete</a></li>
+          <li className="side-menu-item"><a href="#kiosco-1">Kiosco</a></li>
+          <li className="side-menu-item"><a href="#limpieza-1">Limpieza</a></li>
+          <li className="side-menu-item"><a href="#varios-1">Varios</a></li>
+          <li className="side-menu-item"><a href="#te-1">Té</a></li>
+          <li className="side-menu-item"><a href="#yerba-1">Yerba</a></li>
+          <li className="side-menu-item"><a href="#promocion-1">Promocion</a></li>
+        </ul>
+      </nav>
+      <h2 id="menu-toggle" className="no-print menu-toggle">Menu</h2>
     </div>
   );
 }
