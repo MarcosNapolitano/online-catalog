58204b23fb26d108602457ecd587145251f9703d
Marcos Napolitano <marcosnapolitano@hotmail.com.ar>
2025-12-22 16:47:10 +0100

    Fix: Admin panel bug and updated remote URLS

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,12 @@ const nextConfig: NextConfig = {
   },
   images: {
     remotePatterns: [new URL("https://drive.google.com/file/d/**")],
+    domains: ["https://raw.githubusercontent.com"]
   },
 };
+
+if (process.env.NODE_ENV === "production")
+  nextConfig.basePath =
+    "https://raw.githubusercontent.com/MarcosNapolitano/online-catalog/refs/heads/main"
 
 export default nextConfig;
--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,7 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 import { getSession } from '@/app/_services/auth'
 import NotFound from '@/app/not-found'
+import { use } from 'react';
 
 export async function middleware(request: NextRequest) {
 
@@ -9,14 +10,14 @@ export async function middleware(request: NextRequest) {
 
     const isAuthenticated = request.cookies.has('auth_token');
     const user = request.cookies.get('userName')?.value;
-    const isRouteAdmin = request.nextUrl.pathname.startsWith("./admin");
-    const isRouteApi = request.nextUrl.pathname.startsWith("./api");
+    const isRouteAdmin = request.nextUrl.pathname.startsWith("/admin");
+    const isRouteApi = request.nextUrl.pathname.startsWith("/api");
 
     if (!isAuthenticated) {
       return NextResponse.redirect(new URL('/login', request.url));
     };
 
-    if (user != "marcos" && (isRouteAdmin || isRouteApi)){
+    if (user !== "marcos" && (isRouteAdmin || isRouteApi)) {
       return NextResponse.error()
     };
   };
